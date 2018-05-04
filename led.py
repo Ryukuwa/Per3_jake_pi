3951ad342ba4d224fe26fca08131d3fc835accb4
jwinn0958 <jake.i.winn@gmail.com>
2018-05-04 20:28:55 +0000

    Led Flash:

---
+++ led.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+from time import sleep
+
+sunshine = LED(17)
+
+while True:
+    print("LED on")
+    sunshine.on()
+    sleep(1)
+    print("LED off")
+    sunshine.off()
+    sleep(1.5)
